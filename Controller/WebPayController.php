493e721fd4e705d1e5c7d3aba24da72236ba8ddd
Javier Godoy Núñez <rotvulpix@gmail.com>
2014-07-11 00:47:26 -0400

    WebPay Controller, core functions

---
+++ Controller/WebPayController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace rotvulpix\Symfony\TransbankBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class WebPayController extends Controller
+{
+    public function CheckoutAction($monto)
+    {
+    	$parametros['monto'] = $monto;
+        return $this->render('rotvulpixSymfonyTransbankBundle:WebPay:checkout.html.twig', $parametros);
+    }
+
+    public function ExitoAction()
+    {
+        return $this->render('rotvulpixSymfonyTransbankBundle:WebPay:exito.html.twig');
+    }
+
+    public function FracasoAction()
+    {
+        return $this->render('rotvulpixSymfonyTransbankBundle:WebPay:fracaso.html.twig');
+    }
+
+    public function CierreAction()
+    {
+        return new Response('ACEPTADO');
+    }
+}
