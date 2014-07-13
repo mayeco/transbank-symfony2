9a85046782f2c9d136e611481f972a8ef91c0498
Javier Godoy Núñez <rotvulpix@gmail.com>
2014-07-13 01:25:35 -0400

    Creación de Página de Éxito con datos de prueba, aún sin lógica

--- Controller/WebPayController.php
+++ Controller/WebPayController.php
@@ -4,7 +4,8 @@ namespace rotvulpix\Symfony\TransbankBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-
+use rotvulpix\Symfony\TransbankBundle\Services\ItemTransaccion;
+use rotvulpix\Symfony\TransbankBundle\Services\Transaccion;
 
 class WebPayController extends Controller
 {
@@ -16,7 +17,21 @@ class WebPayController extends Controller
 
     public function ExitoAction()
     {
-        return $this->render('rotvulpixSymfonyTransbankBundle:WebPay:exito.html.twig');
+        // Nuevo Ítem de Transacción
+        $transaccion = new Transaccion();
+
+        // Ítems
+        $manzanas = new ItemTransaccion(3990, 'Caja de Manzanas', 4);
+
+        // Agregar Ítems
+        $transaccion->addItem($manzanas);
+
+        // Añadimos Transacción al Render
+        $parametros['transaccion'] = $transaccion;
+
+        // Render
+        return $this->render('rotvulpixSymfonyTransbankBundle:WebPay:exito.html.twig', $parametros);
+        
     }
 
     public function FracasoAction()
